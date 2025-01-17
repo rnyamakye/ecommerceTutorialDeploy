0aacf9fd05c6cc614a90a97f4ed2ad00c19b49d4
Richard Nyamekye <rnya2022@gmail.com>
2025-01-17 13:07:41 +0000

    backend uploaded

--- client/src/config.ts
+++ client/src/config.ts
@@ -7,7 +7,8 @@ const checkConfig = (server: string): Config | {} => {
   switch (server) {
     case "production":
       config = {
-        baseURL: "http://localhost:8000"
+        baseURL:
+          "https://ecommercebackend--ecommerce-tutorial-817f2.asia-east1.hosted.app",
       };
       break;
     case "local":
